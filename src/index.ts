b56c9ec8ead6c2c1e31922d9cd47193fdff858ab
Lizaveta Petrova <goldglaider@gmail.com>
2022-11-13 12:47:27 +0300

    fix: fix file way

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${Lizaveta}:${hkJRt7OBgipBQYyR}@${kanbanapp.3hp1por}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://Lizaveta:hkJRt7OBgipBQYyR@kanbanapp.3hp1por.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
